e769367d66f8633760aaa6c136ba5bb40e601835
Gian <gianfranco376@gmail.com>
2018-02-01 03:04:31 -0300

    Practica 0 completa

--- practica_0.hs
+++ practica_0.hs
@@ -1,10 +1,6 @@
 module Practica0 where
--- Auxiliares --
-esPrimo:: Int -> Bool
-esPrimo x = length (divisores x) == 2 
 
-divisores:: Int -> [Int]
-divisores x = [ y | y <- [1..x], x `mod` y == 0 ]   
+import Data.List
 
 -- Ejercicio 2 --
 valorAbsoluto :: Float -> Float
@@ -19,7 +15,8 @@ factorial 1 = 1
 factorial x = x * factorial (x-1)
 
 cantDivisoresPrimos :: Int -> Int
-cantDivisoresPrimos x = length (filter esPrimo (divisores x));
+cantDivisoresPrimos x = length (filter (\x -> length (divisores x) == 2) (divisores x))
+    where divisores x = [ y | y <- [1..x], x `mod` y == 0 ];
 
 -- Ejercicio 3 --
 inverso :: Float -> Maybe Float
@@ -34,3 +31,25 @@ aEntero (Right x) | x == True = 1
 -- Ejercicio 4 --
 limpiar :: String -> String -> String
 limpiar xs ys = [ y | y <- ys, not(elem y xs) ]
+
+difPromedio :: [Float] -> [Float]
+difPromedio xs = map (\y -> y - promedio xs) xs 
+    where promedio xs = (sum xs) / (genericLength xs)
+
+todosIguales :: [Int] -> Bool
+todosIguales xs = foldr (\y  rec -> ((length xs == 1) || (y == (head xs))) && rec) True xs
+
+-- Ejercicio 5 --
+data AB a = Nil | Bin (AB a) a (AB a) deriving Show
+
+vacioAB:: AB a -> Bool
+vacioAB Nil = True
+vacioAB (Bin _ _ _) = False
+
+negacionAB :: AB Bool -> AB Bool
+negacionAB Nil = Nil
+negacionAB (Bin l x r) = Bin (negacionAB l) (not x) (negacionAB r)
+
+productoAB :: AB Int -> Int
+productoAB Nil = 1
+productoAB (Bin l x r) = x * (productoAB l) * (productoAB r)
