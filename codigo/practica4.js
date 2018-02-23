462389c5f6d2a9f884cf1bcaaf43296fa547171f
Gian150 <gianfranco376@gmail.com>
2018-02-23 00:46:50 -0300

    Fue

---
+++ codigo/practica4.js
@@ -0,0 +1,14 @@
+let complejUnitario = { real: 1, imagianaria: 1};
+
+c1i.sumar = function(otroComplejo) {
+	this.real += otroComplejo.real;
+	this.imagianaria += otroComplejo.imagianaria;
+	return this.
+}
+
+
+c1i.sumar2 = function(otroComplejo) {
+	this.real += otroComplejo.real;
+	this.imagianaria += otroComplejo.imagianaria;
+	return this.
+}
